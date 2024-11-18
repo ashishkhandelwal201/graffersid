900009d1922b07f5284cd63fe9cdbb1e0d528793
Aashish Khandelwal <128039540+ashishkhandelwal201@users.noreply.github.com>
2024-11-18 21:25:24 +0530

    router.js

--- graffersid/routes/router.js
+++ graffersid/routes/router.js
@@ -4,14 +4,15 @@ const router = express.Router()
 router.get('/' , (req,res) => res.render("login") )
 router.get('/login' , userController.getLogin)
 router.post('/login' , userController.postLogin)
+router.get('/signup' , userController.getSignup)
 router.post('/signup' , userController.postSignup)
 router.get('/logout'  , userController.logout )
 
 
-router.get('/companies'  , companiesController.getCompanies )
+router.get('/companies'  , verifyAuth , companiesController.getCompanies )
 router.post('/add-companies'  , companiesController.addCompanies )
 
-router.get('/reviews/:company_id' , reviewsController.getReviews )
+router.get('/reviews/:company_id' , verifyAuth , reviewsController.getReviews )
 router.post('/addreviews/:company_id' ,  reviewsController.addReviews )
 
 
